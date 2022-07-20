cd952dd42084615f495fe5c7192620e2d557bacd
Rex Tsou <akccakccwww@gmail.com>
2022-07-21 00:19:21 +0800

    feat: zsh_better_pnpm_completion

--- README.md
+++ README.md
@@ -1,29 +1,29 @@
-# zsh-better-npm-completion
+# zsh-better-pnpm-completion
 
-> Better completion for `npm`
+> Better completion for `pnpm`
 
 <img src="demo.gif" width="690">
 
-* Makes `npm install` recommendations from npm cache
-* Makes `npm uninstall` recommendations from `dependencies`/`devDependencies`
-* Shows detailed information on script contents for `npm run`
-* Falls back to default npm completions if we don't have anything better
+* Makes `pnpm install` recommendations from pnpm cache
+* Makes `pnpm uninstall` recommendations from `dependencies`/`devDependencies`
+* Shows detailed information on script contents for `pnpm run`
+* Falls back to default pnpm completions if we don't have anything better
 
 ## Installation
 
 ### Using [Antigen](https://github.com/zsh-users/antigen)
 
-Bundle `zsh-better-npm-completion` in your `.zshrc`
+Bundle `zsh-better-pnpm-completion` in your `.zshrc`
 
 ```shell
-antigen bundle lukechilds/zsh-better-npm-completion
+antigen bundle akccakcctw/zsh-better-pnpm-completion
 ```
 
 ### Using [zplug](https://github.com/b4b4r07/zplug)
-Load `zsh-better-npm-completion` as a plugin in your `.zshrc`
+Load `zsh-better-pnpm-completion` as a plugin in your `.zshrc`
 
 ```shell
-zplug "lukechilds/zsh-better-npm-completion", defer:2
+zplug "akccakcctw/zsh-better-pnpm-completion", defer:2
 
 ```
 ### Using [zgen](https://github.com/tarjoilija/zgen)
@@ -31,39 +31,34 @@ zplug "lukechilds/zsh-better-npm-completion", defer:2
 Include the load command in your `.zshrc`
 
 ```shell
-zgen load lukechilds/zsh-better-npm-completion
+zgen load akccakcctw/zsh-better-pnpm-completion
 ```
 
 ### As an [Oh My ZSH!](https://github.com/robbyrussell/oh-my-zsh) custom plugin
 
-Clone `zsh-better-npm-completion` into your custom plugins repo
+Clone `zsh-better-pnpm-completion` into your custom plugins repo
 
 ```shell
-git clone https://github.com/lukechilds/zsh-better-npm-completion ~/.oh-my-zsh/custom/plugins/zsh-better-npm-completion
+git clone https://github.com/akccakcctw/zsh-better-pnpm-completion ~/.oh-my-zsh/custom/plugins/zsh-better-pnpm-completion
 ```
 Then load as a plugin in your `.zshrc`
 
 ```shell
-plugins+=(zsh-better-npm-completion)
+plugins+=(zsh-better-pnpm-completion)
 ```
 
 ### Manually
-Clone this repository somewhere (`~/.zsh-better-npm-completion` for example)
+Clone this repository somewhere (`~/.zsh-better-pnpm-completion` for example)
 
 ```shell
-git clone https://github.com/lukechilds/zsh-better-npm-completion.git ~/.zsh-better-npm-completion
+git clone https://github.com/akccakcctw/zsh-better-pnpm-completion.git ~/.zsh-better-pnpm-completion
 ```
 Then source it in your `.zshrc`
 
 ```shell
-source ~/.zsh-better-npm-completion/zsh-better-npm-completion.plugin.zsh
+source ~/.zsh-better-pnpm-completion/zsh-better-pnpm-completion.plugin.zsh
 ```
-
-## Related
-
-- [`zsh-nvm`](https://github.com/lukechilds/zsh-nvm) - Zsh plugin for installing, updating and loading `nvm`
-- [`gifgen`](https://github.com/lukechilds/gifgen) - Simple high quality GIF encoding 
 
 ## License
 
-MIT © Luke Childs
+MIT © RexTsou
--- zsh-better-pnpm-completion.plugin.zsh
+++ zsh-better-pnpm-completion.plugin.zsh
@@ -1,20 +1,20 @@
-_zbnc_npm_command() {
+_zbpc_pnpm_command() {
   echo "${words[2]}"
 }
 
-_zbnc_npm_command_arg() {
+_zbpc_pnpm_command_arg() {
   echo "${words[3]}"
 }
 
-_zbnc_no_of_npm_args() {
+_zbpc_no_of_pnpm_args() {
   echo "$#words"
 }
 
-_zbnc_list_cached_modules() {
-  ls ~/.npm 2>/dev/null
+_zbpc_list_cached_modules() {
+  ls $(pnpm store path) 2>/dev/null
 }
 
-_zbnc_recursively_look_for() {
+_zbpc_recursively_look_for() {
   local filename="$1"
   local dir=$PWD
   while [ ! -e "$dir/$filename" ]; do
@@ -24,7 +24,7 @@ _zbnc_recursively_look_for() {
   [[ ! "$dir" = "" ]] && echo "$dir/$filename"
 }
 
-_zbnc_get_package_json_property_object() {
+_zbpc_get_package_json_property_object() {
   local package_json="$1"
   local property="$2"
   cat "$package_json" |
@@ -33,72 +33,72 @@ _zbnc_get_package_json_property_object() {
     sed -E 's/    "([^"]+)": "(.+)",?/\1=>\2/'      # Parse into key=>value
 }
 
-_zbnc_get_package_json_property_object_keys() {
+_zbpc_get_package_json_property_object_keys() {
   local package_json="$1"
   local property="$2"
-  _zbnc_get_package_json_property_object "$package_json" "$property" | cut -f 1 -d "="
+  _zbpc_get_package_json_property_object "$package_json" "$property" | cut -f 1 -d "="
 }
 
-_zbnc_parse_package_json_for_script_suggestions() {
+_zbpc_parse_package_json_for_script_suggestions() {
   local package_json="$1"
-  _zbnc_get_package_json_property_object "$package_json" scripts |
+  _zbpc_get_package_json_property_object "$package_json" scripts |
     sed -E 's/(.+)=>(.+)/\1:$ \2/' |  # Parse commands into suggestions
     sed 's/\(:\)[^$]/\\&/g' |         # Escape ":" in commands
     sed 's/\(:\)$[^ ]/\\&/g'          # Escape ":$" without a space in commands
 }
 
-_zbnc_parse_package_json_for_deps() {
+_zbpc_parse_package_json_for_deps() {
   local package_json="$1"
-  _zbnc_get_package_json_property_object_keys "$package_json" dependencies
-  _zbnc_get_package_json_property_object_keys "$package_json" devDependencies
+  _zbpc_get_package_json_property_object_keys "$package_json" dependencies
+  _zbpc_get_package_json_property_object_keys "$package_json" devDependencies
 }
 
-_zbnc_npm_install_completion() {
+_zbpc_pnpm_install_completion() {
 
-  # Only run on `npm install ?`
-  [[ ! "$(_zbnc_no_of_npm_args)" = "3" ]] && return
+  # Only run on `pnpm install ?`
+  [[ ! "$(_zbpc_no_of_pnpm_args)" = "3" ]] && return
 
   # Return if we don't have any cached modules
-  [[ "$(_zbnc_list_cached_modules)" = "" ]] && return
+  [[ "$(_zbpc_list_cached_modules)" = "" ]] && return
 
   # If we do, recommend them
-  _values $(_zbnc_list_cached_modules)
+  _values $(_zbpc_list_cached_modules)
 
   # Make sure we don't run default completion
   custom_completion=true
 }
 
-_zbnc_npm_uninstall_completion() {
+_zbpc_pnpm_uninstall_completion() {
 
-  # Use default npm completion to recommend global modules
-  [[ "$(_zbnc_npm_command_arg)" = "-g" ]] ||  [[ "$(_zbnc_npm_command_arg)" = "--global" ]] && return
+  # Use default pnpm completion to recommend global modules
+  [[ "$(_zbpc_pnpm_command_arg)" = "-g" ]] || [[ "$(_zbpc_pnpm_command_arg)" = "--global" ]] && return
 
   # Look for a package.json file
-  local package_json="$(_zbnc_recursively_look_for package.json)"
+  local package_json="$(_zbpc_recursively_look_for package.json)"
 
   # Return if we can't find package.json
   [[ "$package_json" = "" ]] && return
 
-  _values $(_zbnc_parse_package_json_for_deps "$package_json")
+  _values $(_zbpc_parse_package_json_for_deps "$package_json")
 
   # Make sure we don't run default completion
   custom_completion=true
 }
 
-_zbnc_npm_run_completion() {
+_zbpc_pnpm_run_completion() {
 
-  # Only run on `npm run ?`
-  [[ ! "$(_zbnc_no_of_npm_args)" = "3" ]] && return
+  # Only run on `pnpm run ?`
+  [[ ! "$(_zbpc_no_of_pnpm_args)" = "3" ]] && return
 
   # Look for a package.json file
-  local package_json="$(_zbnc_recursively_look_for package.json)"
+  local package_json="$(_zbpc_recursively_look_for package.json)"
 
   # Return if we can't find package.json
   [[ "$package_json" = "" ]] && return
 
   # Parse scripts in package.json
   local -a options
-  options=(${(f)"$(_zbnc_parse_package_json_for_script_suggestions $package_json)"})
+  options=(${(f)"$(_zbpc_parse_package_json_for_script_suggestions $package_json)"})
 
   # Return if we can't parse it
   [[ "$#options" = 0 ]] && return
@@ -110,34 +110,34 @@ _zbnc_npm_run_completion() {
   custom_completion=true
 }
 
-_zbnc_default_npm_completion() {
+_zbpc_default_pnpm_completion() {
   compadd -- $(COMP_CWORD=$((CURRENT-1)) \
               COMP_LINE=$BUFFER \
               COMP_POINT=0 \
-              npm completion -- "${words[@]}" \
+              pnpm completion -- "${words[@]}" \
               2>/dev/null)
 }
 
-_zbnc_zsh_better_npm_completion() {
+_zbpc_zsh_better_pnpm_completion() {
 
   # Store custom completion status
   local custom_completion=false
 
   # Load custom completion commands
-  case "$(_zbnc_npm_command)" in
+  case "$(_zbpc_pnpm_command)" in
     i|install)
-      _zbnc_npm_install_completion
+      _zbpc_pnpm_install_completion
       ;;
-    r|uninstall)
-      _zbnc_npm_uninstall_completion
+    remove|uninstall)
+      _zbpc_pnpm_uninstall_completion
       ;;
     run)
-      _zbnc_npm_run_completion
+      _zbpc_pnpm_run_completion
       ;;
   esac
 
   # Fall back to default completion if we haven't done a custom one
-  [[ $custom_completion = false ]] && _zbnc_default_npm_completion
+  [[ $custom_completion = false ]] && _zbpc_default_pnpm_completion
 }
 
-compdef _zbnc_zsh_better_npm_completion npm
+compdef _zbpc_zsh_better_pnpm_completion pnpm
